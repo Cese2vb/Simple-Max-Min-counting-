9b80eeb03899a30e36bb97b37ca2a350f8c5339d
cesia <cesiaevv@gmail.com>
2016-10-27 21:13:32 -0700

    Add files via upload

---
+++ counting.py
@@ -0,0 +1,21 @@
+largest = None
+smallest = None
+num_list=[]
+while True:
+    num = raw_input("Enter a number: ")
+ 
+    if num == "done": 
+    	break
+           
+    try:
+		num=int(num)
+		num_list.append(num)
+		largest=max(num_list)
+		smallest=min(num_list)
+           
+    except:
+       	print "invalid input"
+ 
+ 
+print "Maximum is", largest
+print "Minimum is", smallest
